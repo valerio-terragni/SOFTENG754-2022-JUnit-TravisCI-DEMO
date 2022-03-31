d0cf537e3dc24768578e22cefadb6d34368fc5b5
Valerio Terragni <v.terragni@auckland.ac.nz>
2022-04-01 06:25:09 +0800

    add test cases and the badge

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # SE754 2022
 # Demo JUnit + TravisCI
 
-
+[![Build Status](https://app.travis-ci.com/valerio-terragni/SOFTENG754-2022-JUnit-TravisCI-DEMO.svg?branch=main)](https://app.travis-ci.com/valerio-terragni/SOFTENG754-2022-JUnit-TravisCI-DEMO)
---
+++ src/test/java/nz/ac/auckland/se754/BankAccountTest.java
@@ -0,0 +1,47 @@
+package nz.ac.auckland.se754;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BankAccountTest {
+    BankAccount bankAccount;
+    @BeforeEach
+    public void setup(){
+        bankAccount = new BankAccount();
+        bankAccount.deposit(100);
+    }
+
+    @Test
+    public void test_deposit(){
+
+        double actual = bankAccount.getBalance();
+        assertEquals(100,actual,0.00000001);
+    }
+
+    @Test
+    public void test_withdraw(){
+        bankAccount.withdraw(10);
+        double actual = bankAccount.getBalance();
+        assertEquals(90,actual,0.00000001);
+    }
+
+    @Test
+    public void test_withdraw_exception_not(){
+        try {
+            bankAccount.withdraw(1);
+        }catch(BalanceNotEnoughException e){
+            fail();
+        }
+
+    }
+    @Test
+    public void test_withdraw_exception(){
+
+        assertThrows(BalanceNotEnoughException.class,  () -> {bankAccount.withdraw(1000);});
+
+    }
+
+}
